ceeabfb65928bc3f7d771559555d84ea441065d0
cesarsarabia <cesar.rsarabia@alumnos.udg.mx>
2020-04-14 19:55:49 -0500

    Vista home Modificada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,63 @@
-@extends('layouts.app')
+@extends('layouts.main')
+
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+
+<!-- Start Slider Area -->
+<div class="slider__container slider--one bg__cat--3">
+    <div class="slide__container slider__activation__wrap owl-carousel">
+        <!-- Start Single Slide -->
+        <div class="single__slide animation__style01 slider__fixed--height">
+            <div class="container">
+                <div class="row align-items__center">
+                    <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                        <div class="slide">
+                            <div class="slider__inner">
+                                <h2>Nuevos Sabores 2020</h2>
+                                <h1>Crispy!</h1>
+                                <div class="cr__btn">
+                                    <a href="cart.html">Shop Now</a>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
+                    <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                        <div class="slide__thumb">
+                            <img src="images/slider/fornt-img/1.png" alt="slider images">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- End Single Slide -->
+        <!-- Start Single Slide -->
+        <div class="single__slide animation__style01 slider__fixed--height">
+            <div class="container">
+                <div class="row align-items__center">
+                    <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                        <div class="slide">
+                            <div class="slider__inner">
+                                <h2>Marcas exclusivas</h2>
+                                <h1>Exclusive!</h1>
+                                <div class="cr__btn">
+                                    <a href="cart.html">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                        <div class="slide__thumb">
+                            <img src="images/slider/fornt-img/2.png" alt="slider images">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Single Slide -->
     </div>
 </div>
+<!-- Start Slider Area -->
+
+
 @endsection
